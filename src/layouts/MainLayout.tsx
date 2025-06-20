8854c2a21fc46aaff27914c32a1fdca842cd58ba
Md Nakibul Hosen Nahid Nahid <nakibulhosen7@gmail.com>
2025-06-20 12:17:05 +0600

    ADD: create MainLayout component for application layout structure

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
+import Topbar from "../components/Topbar"
+
+export default function MainLayout() {
+    return (
+        <div className="flex h-screen">
+            <Sidebar />
+            <div className="flex flex-col flex-1 overflow-hidden">
+                <Topbar />
+                <main className="p-4 overflow-y-auto">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    )
+}
