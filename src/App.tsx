d9364076dde033ec1878c97655a8aa85e3daaba6
Md Nakibul Hosen Nahid Nahid <nakibulhosen7@gmail.com>
2025-06-24 11:48:28 +0600

    ADD: update routing in App component to include Login and NotFound pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Patients from "./pages/Patients"
 import Appointments from "./pages/Appointments"
 import Settings from "./pages/Settings"
 import MainLayout from "./layouts/MainLayout"
 import PatientView from "./pages/PatientView"
+import Login from "@/pages/Login"
+import NotFound from "@/pages/NotFound"
 // TODO: Review icon library. We have 3 library for icon but how many are actually needed? 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
         <Route path="appointments" element={<Appointments />} />
         <Route path="settings" element={<Settings />} />
         <Route path="/patients/:id" element={<PatientView />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   )
